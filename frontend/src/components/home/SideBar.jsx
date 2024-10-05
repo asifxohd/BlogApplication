4aaa60352b32b846973cafc4dfbc12fe312992d1
Muhammed Asif <140389446+asifxohd@users.noreply.github.com>
2024-10-05 10:44:10 +0530

    resolved issue on import issue on sidebar for production

--- frontend/src/components/home/SideBar.jsx
+++ frontend/src/components/home/SideBar.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Sidebar, SidebarBody, SidebarLink } from "../sidebarComponents/sideutils";
 import {
     IconFilePencil,
     IconLogout2,
@@ -13,6 +12,7 @@ import { cn } from "../../utils/lib/utils";
 import ConfirmationModal from './LogoutModal';
 import { toast } from "react-toastify";
 import axiosInstance from "../../axios/axiosIntrecepters";
+import { Sidebar, SidebarBody, SidebarLink } from "../sidebarComponents/sideutils";
 
 export function SidebarDemo({ children }) {
     const [data, setData] = useState({
